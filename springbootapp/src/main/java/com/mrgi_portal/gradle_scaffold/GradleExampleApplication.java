791b5deaaaf6bb4883710b7ee35259b000c6e683
CharlesDerek <charlesderek177@gmail.com>
2023-06-19 15:37:41 -0400

    re-build architecture for compose hybrid cloud paradigm (Athernex) (k8s, compose, Docker, and Gradle)

---
+++ springbootapp/src/main/java/com/mrgi_portal/gradle_scaffold/GradleExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mrgi_portal.gradle_scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleExampleApplication.class, args);
+	}
+
+}
